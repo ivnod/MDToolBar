249ca643ec08e91b2803e3cb13f166a7e9e534a3
vinod medagam <vinod.medagam@kony.com>
2018-11-28 18:13:49 +0700

    adding menu popup to toolbar

--- app/src/main/java/com/example/ksi074/mdtoolbar/MainActivity.java
+++ app/src/main/java/com/example/ksi074/mdtoolbar/MainActivity.java
@@ -1,13 +1,40 @@
 package com.example.ksi074.mdtoolbar;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.item1) {
+            Toast.makeText(this, "item " + item.getTitle(), Toast.LENGTH_LONG).show();
+        } else if (id == R.id.item2) {
+            Toast.makeText(this, "item " + item.getTitle(), Toast.LENGTH_LONG).show();
+        } else if (id == R.id.item3) {
+            Toast.makeText(this, "item " + item.getTitle(), Toast.LENGTH_LONG).show();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
